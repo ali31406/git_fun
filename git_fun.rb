064c4f730f2a32777ec1445824552c1f0cb5fd4c
Ifiok <ifiudowana@gmail.com>
2018-06-28 03:48:35 +0100

    first check in

---
+++ git_fun.rb
@@ -0,0 +1,26 @@
+#!/cygdrive/c/tools/ruby25/bin/ruby
+
+require 'optparse'
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: ruby_fun.rb [options]"
+
+# no argument required
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+# argument required
+  opts.on("-pPATH", "--path", "Path to Repo",:reqiured ) do |path|
+    options[:path] = path
+  end
+
+end.parse!
+fail "Path not provided" unless options[:path]
+my_path = options[:path]
+Dir.chdir my_path
+my_string = `git branch -a`
+puts "string is: " + my_string
+lines = my_string.chomp.split(/\n/)
+puts lines
+
